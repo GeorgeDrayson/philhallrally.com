3f781a420bf859112ffa75f77f949bbc21cc1013
George Drayson <drayson.g@mac.com>
2019-07-25 19:22:37 +0100

    made calendar page, has resizing issues

--- script.js
+++ script.js
@@ -6,24 +6,33 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 500, function(){
+      }, 600, function(){
 
         window.location.hash = hash;
       });
     }
   });
+
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
 });
 
 $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 580) {
-      $(".container").animate({
-        backgroundColor: 'rgba(0,0,0,0.5)'}, 10);
+      $(".navbar").animate({
+        backgroundColor: 'rgba(0,0,0,0.5)'}, 20);
 	  }
 
 	  else{
-      $(".container").animate({
+      $(".navbar").animate({
         backgroundColor: 'transparent'}, 1);
     }
   });
