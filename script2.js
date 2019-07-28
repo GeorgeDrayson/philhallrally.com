408a37ff7eb6654426f8ab27f4fc3985b396a782
George Drayson <drayson.g@mac.com>
2019-07-28 19:35:13 +0100

    Fixed calendar page, is now fully functioning with resizable background image

--- script2.js
+++ script2.js
@@ -12,5 +12,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "inline-block";
+  slides[slideIndex-1].style.display = "block";
 }
