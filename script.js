f59f335848bcede8591fa09c47da08d6b1e1b809
George Drayson <drayson.g@mac.com>
2019-07-29 12:50:09 +0100

    cleaning up index.html

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
+    document.getElementById("progress-bar").style.width = scrolled + "%";
   }
 });
 
